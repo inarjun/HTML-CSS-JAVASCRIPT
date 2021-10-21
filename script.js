85f51691e2c5c25671d1fb848cf3dff58a111886
Arjun Kumar <57954125+inarjun@users.noreply.github.com>
2021-10-21 23:37:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,65 +1,34 @@
-const data = {
-  mainTitle: "My Favourite Cloud Platform",
-  cardContents: [
-    {
-      name: "Google Cloud",
-      imageLink:
-        "https://logos-world.net/wp-content/uploads/2021/02/Google-Cloud-Emblem.png",
-    },
-    {
-      name: "Amazon Cloud",
-      imageLink:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/1200px-Amazon_Web_Services_Logo.svg.png",
-    },
-    {
-      name: "Microsoft Azure",
-      imageLink:
-        "https://i0.wp.com/dailydotnettips.com/wp-content/uploads/2021/05/Azure-Logo.png?resize=783%2C426&ssl=1",
-    },
-    {
-      name: "Oracle Cloud",
-      imageLink:
-        "https://asycuda.org/wp-content/uploads/logo-oracle-256.png",
-    },
-  ],
-  userData: { name: "ARJUN KUMAR", tagLine: "Baby steps into the world of web!" },
-};
+let usernameElement = document.getElementById("username")
+let passwordElement = document.getElementById("password");
+let loginButtonElement = document.getElementById("login-button");
+
+passwordElement.addEventListener('blur',
+    () => {
+        let password = passwordElement.value;
+        if (password.length < 6) {
+            document.getElementById("message").style.display = "block";
+        } else {
+            document.getElementById("message").style.display = "none";
+        }
+    }
+)
+
+let formElement = document.getElementById("login-form");
+formElement.addEventListener('focusout', () => {
+    console.log('focusout')
+    let username = usernameElement.value;
+    let password = passwordElement.value;
+
+    if (isValidUsername(username) && isValidPassword(password)) {
+        loginButtonElement.disabled = false;
+        loginButtonElement.classList.remove("disabled")
+    } else {
+        loginButtonElement.disabled = true;
+        loginButtonElement.classList.add("disabled")
+    }
+})
 
 
-// Plug in title data to HTML
-let mainTitleElement = document.getElementById("main-title");
-mainTitleElement.innerHTML += data.mainTitle;
+let isValidUsername = username => username.length > 0
+let isValidPassword = password => password.length >= 6
 
-
-// Create template for card content
-let getCardStructure = (name, imageLink) => {
-  return `<div class="col-12 col-sm-6 mb-4">
-  <div class="tile">
-    <div class="tile-text text-center">
-      <h5>${name}</h5>
-    </div>
-    <img 
-      src="${imageLink}" />
-  </div>
-</div>`;
-};
-
-let cardRow = document.getElementById("card-row");
-
-// Plug in card data to HTML
-data.cardContents.forEach((cardContent) => {
-  let { name, imageLink } = cardContent;
-  cardRow.innerHTML += getCardStructure(name, imageLink);
-});
-
-// Create template for footer content
-let getFooterStructure = (name, tagLine) => {
-  return `<h3 class="me-3"><i class="fas fa-heart"></i> ${name}</h3>
-    <p>${tagLine}</p>`;
-};
-
-let footerContent = document.getElementById("footer");
-let { name, tagLine } = data.userData;
-
-// Plug in footer data to HTML
-footerContent.innerHTML += getFooterStructure(name, tagLine);
